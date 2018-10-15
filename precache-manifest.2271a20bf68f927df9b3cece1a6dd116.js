2905e530747fdca1569f3af7dde48bebaf61f5cf
sakiko <sakiko.ue@gmail.com>
2018-10-16 11:12:07 +1300

    Updates

---
+++ precache-manifest.2271a20bf68f927df9b3cece1a6dd116.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "c9a5f4a813a7971c6021",
+    "url": "/weather/static/js/main.c9a5f4a8.chunk.js"
+  },
+  {
+    "revision": "97b4b58ee1d73b8df0cb",
+    "url": "/weather/static/js/1.97b4b58e.chunk.js"
+  },
+  {
+    "revision": "c9a5f4a813a7971c6021",
+    "url": "/weather/static/css/main.ef605293.chunk.css"
+  },
+  {
+    "revision": "66cfeb7e50b3f54335e61d321c4b90b8",
+    "url": "/weather/index.html"
+  }
+];
